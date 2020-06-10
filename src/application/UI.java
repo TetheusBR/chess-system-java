389e650fadef5bda584a0d88385ca3d3021289f7
TetheusBR <matheusdf04@gmail.com>
2020-06-10 17:03:04 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,4 +1,3 @@
-
 package application;
 
 import java.util.Arrays;
@@ -58,15 +57,7 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		if (!chessMatch.getCheckMate()) {
-			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-			if (chessMatch.getCheck()) {
-				System.out.println("CHECK!");
-			}
-		} else {
-			System.out.println("CHECKMATE!");
-			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
-		}
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -26,12 +26,13 @@ public class Position {
 		this.column = column;
 	}
 
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
 	@Override
 	public String toString() {
 		return row + ", " + column;
-	}
-
-	public void setValues(int row2, int i) {
-		
 	}
 }
